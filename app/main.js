1397686cb0cb119b567cda199b492de3e1754342
Aleksei Kuzmichev <aleksei.kuzmichev@my.games>
2023-10-20 19:08:19 +0600

    oops

--- app/main.js
+++ app/main.js
@@ -19,12 +19,12 @@ function main() {
   const command = process.argv[2];
 
   // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here!");
+  // console.log("Logs from your program will appear here!");
 
   // Uncomment this block to pass the first stage
   if (command === "decode") {
     const bencodedValue = process.argv[3];
-  
+
     // In JavaScript, there's no need to manually convert bytes to string for printing
     // because JS doesn't distinguish between bytes and strings in the same way Python does.
     console.log(JSON.stringify(decodeBencode(bencodedValue)));
