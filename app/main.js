670c8121acc0fafee30666975689e0fa4ab7eb20
Aleksei Kuzmichev <aleksei.kuzmichev@my.games>
2023-10-20 18:43:43 +0600

    uncommented code

--- app/main.js
+++ app/main.js
@@ -22,15 +22,15 @@ function main() {
   console.log("Logs from your program will appear here!");
 
   // Uncomment this block to pass the first stage
-  // if (command === "decode") {
-  //   const bencodedValue = process.argv[3];
-  //
-  //   // In JavaScript, there's no need to manually convert bytes to string for printing
-  //   // because JS doesn't distinguish between bytes and strings in the same way Python does.
-  //   console.log(JSON.stringify(decodeBencode(bencodedValue)));
-  // } else {
-  //   throw new Error(`Unknown command ${command}`);
-  // }
+  if (command === "decode") {
+    const bencodedValue = process.argv[3];
+  
+    // In JavaScript, there's no need to manually convert bytes to string for printing
+    // because JS doesn't distinguish between bytes and strings in the same way Python does.
+    console.log(JSON.stringify(decodeBencode(bencodedValue)));
+  } else {
+    throw new Error(`Unknown command ${command}`);
+  }
 }
 
 main();
