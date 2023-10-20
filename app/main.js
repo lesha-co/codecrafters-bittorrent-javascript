02232f14096db8e460378b77b115812348f69485
Aleksei Kuzmichev <aleksei.kuzmichev@my.games>
2023-10-20 19:59:07 +0600

    test lists

--- app/main.js
+++ app/main.js
@@ -1,44 +1,79 @@
-const process = require("process");
-const util = require("util");
-
-// Examples:
-// - decodeBencode("5:hello") -> "hello"
-// - decodeBencode("10:hello12345") -> "hello12345"
+"use strict";
+// const process = require("node:process");
+const util = require("node:util");
+// type Primitive = string | number;
+// type Primitive2 = Primitive | Primitive[];
+// type Data = Primitive2 & Primitive2[];
+// const d1: Data = 1;
+// const d2: Data = "1";
+// const d3: Data = ["1", 1];
+// const d4: Data = ["1", 1, [1, 2, 3]];
+function consumeOnce(bencodedValue) {
+    // checking for numbers
+    if (bencodedValue.at(0) === "i") {
+        const end = bencodedValue.indexOf("e");
+        if (end === -1) {
+            throw new Error("wrong integer");
+        }
+        const parsedValue = parseInt(bencodedValue.substring(1, end));
+        return {
+            parsedValue,
+            rest: bencodedValue.substring(end),
+        };
+    }
+    // checking for strings
+    if (!isNaN(parseInt(bencodedValue[0]))) {
+        const colonIndex = bencodedValue.indexOf(":");
+        const dataLength = parseInt(bencodedValue.substring(0, colonIndex));
+        const totalLengthToParse = colonIndex + dataLength + 1;
+        const parsedValue = bencodedValue.substring(colonIndex + 1, totalLengthToParse);
+        return { parsedValue, rest: bencodedValue.substring(totalLengthToParse) };
+    }
+    //checking for lists
+    if (bencodedValue.at(0) === "l") {
+        let unconsumed = bencodedValue.substring(1);
+        const list = [];
+        while (true) {
+            const { parsedValue, rest } = consumeOnce(unconsumed);
+            list.push(parsedValue);
+            if (rest.at(0) === "e") {
+                unconsumed = rest.substring(1);
+                return {
+                    parsedValue: list,
+                    rest: unconsumed,
+                };
+            }
+            else {
+                unconsumed = rest;
+            }
+        }
+    }
+    throw new Error("Datatype not yet supported");
+}
 function decodeBencode(bencodedValue) {
-  if (typeof bencodedValue !== "string") {
-    throw new Error("Only strings are supported at the moment");
-  }
-  // checking for numbers
-  if (bencodedValue.at(0) === "i" && bencodedValue.at(-1) === "e") {
-    return parseInt(bencodedValue.substring(1, bencodedValue.length - 1));
-  }
-  // strings
-  if (!isNaN(bencodedValue[0])) {
-    // Check if the first character is a digit
-    const parts = bencodedValue.split(":");
-    const length = parseInt(parts[0], 10);
-    return parts[1].substr(0, length);
-  } else {
-    throw new Error("Only strings are supported at the moment");
-  }
+    const { parsedValue, rest } = consumeOnce(bencodedValue);
+    if (Array.isArray(parsedValue)) {
+        console.log(`[${parsedValue
+            .map((x) => (typeof x === "number" ? x : `"${x}"`))
+            .join(",")}]`);
+    }
+    else {
+        console.log(parsedValue);
+    }
 }
-
 function main() {
-  const command = process.argv[2];
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  // console.log("Logs from your program will appear here!");
-
-  // Uncomment this block to pass the first stage
-  if (command === "decode") {
-    const bencodedValue = process.argv[3];
-
-    // In JavaScript, there's no need to manually convert bytes to string for printing
-    // because JS doesn't distinguish between bytes and strings in the same way Python does.
-    console.log(JSON.stringify(decodeBencode(bencodedValue)));
-  } else {
-    throw new Error(`Unknown command ${command}`);
-  }
+    const command = process.argv[2];
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // console.log("Logs from your program will appear here!");
+    // Uncomment this block to pass the first stage
+    if (command === "decode") {
+        const bencodedValue = process.argv[3];
+        // In JavaScript, there's no need to manually convert bytes to string for printing
+        // because JS doesn't distinguish between bytes and strings in the same way Python does.
+        console.log(JSON.stringify(decodeBencode(bencodedValue)));
+    }
+    else {
+        throw new Error(`Unknown command ${command}`);
+    }
 }
-
 main();
---
+++ app/main.ts
@@ -0,0 +1,97 @@
+// const process = require("node:process");
+const util = require("node:util");
+
+// type Primitive = string | number;
+// type Primitive2 = Primitive | Primitive[];
+// type Data = Primitive2 & Primitive2[];
+// const d1: Data = 1;
+// const d2: Data = "1";
+// const d3: Data = ["1", 1];
+// const d4: Data = ["1", 1, [1, 2, 3]];
+
+function consumeOnce(bencodedValue: string): {
+  parsedValue: any;
+  rest: string;
+} {
+  // checking for numbers
+  if (bencodedValue.at(0) === "i") {
+    const end = bencodedValue.indexOf("e");
+    if (end === -1) {
+      throw new Error("wrong integer");
+    }
+
+    const parsedValue = parseInt(bencodedValue.substring(1, end));
+    return {
+      parsedValue,
+      rest: bencodedValue.substring(end),
+    };
+  }
+
+  // checking for strings
+  if (!isNaN(parseInt(bencodedValue[0]))) {
+    const colonIndex = bencodedValue.indexOf(":");
+    const dataLength = parseInt(bencodedValue.substring(0, colonIndex));
+    const totalLengthToParse = colonIndex + dataLength + 1;
+
+    const parsedValue = bencodedValue.substring(
+      colonIndex + 1,
+      totalLengthToParse
+    );
+    return { parsedValue, rest: bencodedValue.substring(totalLengthToParse) };
+  }
+
+  //checking for lists
+
+  if (bencodedValue.at(0) === "l") {
+    let unconsumed = bencodedValue.substring(1);
+    const list = [];
+    while (true) {
+      const { parsedValue, rest } = consumeOnce(unconsumed);
+      list.push(parsedValue);
+      if (rest.at(0) === "e") {
+        unconsumed = rest.substring(1);
+        return {
+          parsedValue: list,
+          rest: unconsumed,
+        };
+      } else {
+        unconsumed = rest;
+      }
+    }
+  }
+
+  throw new Error("Datatype not yet supported");
+}
+
+function decodeBencode(bencodedValue: string) {
+  const { parsedValue, rest } = consumeOnce(bencodedValue);
+  if (Array.isArray(parsedValue)) {
+    console.log(
+      `[${parsedValue
+        .map((x) => (typeof x === "number" ? x : `"${x}"`))
+        .join(",")}]`
+    );
+  } else {
+    console.log(parsedValue);
+  }
+}
+
+function main() {
+  const command = process.argv[2];
+
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  // console.log("Logs from your program will appear here!");
+
+  // Uncomment this block to pass the first stage
+  if (command === "decode") {
+    const bencodedValue = process.argv[3];
+
+    // In JavaScript, there's no need to manually convert bytes to string for printing
+    // because JS doesn't distinguish between bytes and strings in the same way Python does.
+    console.log(JSON.stringify(decodeBencode(bencodedValue)));
+  } else {
+    throw new Error(`Unknown command ${command}`);
+  }
+}
+
+main();
