27a51ca3b6d5e8cbaa4a4204b8f0bab6afb07624
Aleksei Kuzmichev <aleksei.kuzmichev@my.games>
2023-10-21 19:12:31 +0600

    support dicts

---
+++ app/decode.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.decodeBencode = void 0;
+function consumeOnce(bencodedValue) {
+    // checking for numbers
+    if (bencodedValue.at(0) === "i") {
+        const end = bencodedValue.indexOf("e");
+        if (end === -1) {
+            throw new Error("wrong integer");
+        }
+        const parsedValue = parseInt(bencodedValue.substring(1, end));
+        return {
+            parsedValue,
+            rest: bencodedValue.substring(end + 1),
+        };
+    }
+    // checking for strings
+    if (!isNaN(parseInt(bencodedValue[0]))) {
+        const colonIndex = bencodedValue.indexOf(":");
+        const dataLength = parseInt(bencodedValue.substring(0, colonIndex));
+        const totalLengthToParse = colonIndex + dataLength + 1;
+        const parsedValue = bencodedValue.substring(colonIndex + 1, totalLengthToParse);
+        return { parsedValue, rest: bencodedValue.substring(totalLengthToParse) };
+    }
+    //checking for lists
+    if (bencodedValue.at(0) === "l") {
+        let unconsumed = bencodedValue.substring(1);
+        const list = [];
+        while (unconsumed.at(0) !== "e") {
+            const { parsedValue, rest } = consumeOnce(unconsumed);
+            list.push(parsedValue);
+            unconsumed = rest;
+        }
+        return {
+            parsedValue: list,
+            rest: unconsumed.substring(1),
+        };
+    }
+    //checking for dicts
+    if (bencodedValue.at(0) === "d") {
+        let unconsumed = bencodedValue.substring(1);
+        const list = [];
+        while (unconsumed.at(0) !== "e") {
+            const { parsedValue, rest } = consumeOnce(unconsumed);
+            list.push(parsedValue);
+            unconsumed = rest;
+        }
+        const dict = {};
+        for (let i = 0; i < list.length; i += 2) {
+            const key = list[i];
+            if (typeof key != "string")
+                continue;
+            dict[key] = list[i + 1];
+        }
+        return {
+            parsedValue: dict,
+            rest: unconsumed.substring(1),
+        };
+    }
+    throw new Error("Datatype not yet supported");
+}
+function decodeBencode(bencodedValue) {
+    const { parsedValue, rest } = consumeOnce(bencodedValue);
+    return parsedValue;
+}
+exports.decodeBencode = decodeBencode;
+//# sourceMappingURL=decode.js.map
--- app/main.js
+++ app/main.js
@@ -1,54 +1,6 @@
 "use strict";
-// const process = require("node:process");
-const util = require("node:util");
-// type Primitive = string | number;
-// type Primitive2 = Primitive | Primitive[];
-// type Data = Primitive2 & Primitive2[];
-// const d1: Data = 1;
-// const d2: Data = "1";
-// const d3: Data = ["1", 1];
-// const d4: Data = ["1", 1, [1, 2, 3]];
-function consumeOnce(bencodedValue) {
-    // checking for numbers
-    if (bencodedValue.at(0) === "i") {
-        const end = bencodedValue.indexOf("e");
-        if (end === -1) {
-            throw new Error("wrong integer");
-        }
-        const parsedValue = parseInt(bencodedValue.substring(1, end));
-        return {
-            parsedValue,
-            rest: bencodedValue.substring(end + 1),
-        };
-    }
-    // checking for strings
-    if (!isNaN(parseInt(bencodedValue[0]))) {
-        const colonIndex = bencodedValue.indexOf(":");
-        const dataLength = parseInt(bencodedValue.substring(0, colonIndex));
-        const totalLengthToParse = colonIndex + dataLength + 1;
-        const parsedValue = bencodedValue.substring(colonIndex + 1, totalLengthToParse);
-        return { parsedValue, rest: bencodedValue.substring(totalLengthToParse) };
-    }
-    //checking for lists
-    if (bencodedValue.at(0) === "l") {
-        let unconsumed = bencodedValue.substring(1);
-        const list = [];
-        while (unconsumed.at(0) !== "e") {
-            const { parsedValue, rest } = consumeOnce(unconsumed);
-            list.push(parsedValue);
-            unconsumed = rest;
-        }
-        return {
-            parsedValue: list,
-            rest: unconsumed.substring(1),
-        };
-    }
-    throw new Error("Datatype not yet supported");
-}
-function decodeBencode(bencodedValue) {
-    const { parsedValue, rest } = consumeOnce(bencodedValue);
-    return parsedValue;
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+const decode_1 = require("./decode");
 function main() {
     const command = process.argv[2];
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -58,7 +10,7 @@ function main() {
         const bencodedValue = process.argv[3];
         // In JavaScript, there's no need to manually convert bytes to string for printing
         // because JS doesn't distinguish between bytes and strings in the same way Python does.
-        console.log(JSON.stringify(decodeBencode(bencodedValue)));
+        console.log(JSON.stringify((0, decode_1.decodeBencode)(bencodedValue)));
     }
     else {
         throw new Error(`Unknown command ${command}`);
